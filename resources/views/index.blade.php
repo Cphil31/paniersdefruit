e37d2c42c3c3dd11d5a53b996d3d2cdf211608b5
Cphil31 <cecilephilippe31@gmail.com>
2017-12-04 14:34:19 +0100

    ajout tableau

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,5 +6,23 @@
 </head>
 <body>
 	<h1>Panier de fruits</h1>
+
+	<table>
+  <tr>
+    <td>&nbsp;</td>
+    <td>fruit</td>
+    <td>Prix</td>
+    <td>Quantité</td>
+    <td>Total</td>
+  </tr>
+  <tr>
+    <td>Race</td>
+    <td>Jack Russell</td>
+    <td>Poodle</td>
+    <td>Streetdog</td>
+  </tr>
+  
+</table>
+
 </body>
 </html>
