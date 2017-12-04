9568fdba425fd60ab5e66352dffb8b6b98042222
Cphil31 <cecilephilippe31@gmail.com>
2017-12-04 17:36:49 +0100

    creation table stock

---
+++ database/migrations/2017_12_04_161541_stock.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Stock extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->decimal('prix');
+            $table->decimal('quantité');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+            
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
